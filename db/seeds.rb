ca7567eb85f136424820ac56f6d09b5eaa62a62d
nana <keyaki.436vrwgz@gmail.com>
2016-09-26 11:01:48 +0900

    seed作成中

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 
-image_file = Fixture::file 'files/image.png'
-
 10.times do |n|
-  Product.create!(name: 'name' + n.to_s, price: 100 + n, sort: 0 + n, is_hidden: false, photo: image_file)
+  Product.create!(name: 'name' + n.to_s, price: 100 + n, sort: 0 + n, is_hidden: false, photo: File.open(File.join(Rails.root, "/test/fixtures/files/image.png")))
 end
