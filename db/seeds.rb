2b7c31a6c89758201524c8851fc9385206b2dd5d
nana <keyaki.436vrwgz@gmail.com>
2016-09-26 10:40:28 +0900

    作業経過コミット。file_fixtures_extension.rbはこの後削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
-image_path = File.join(Rails.root, "test/fixtures/images/image.png")
+image_file = Fixture::file 'files/image.png'
 
 10.times do |n|
-  Product.create!(name: 'name' + n.to_s, price: 100 + n, sort: 0 + n, is_hidden: false, photo: File.new(image_path))
+  Product.create!(name: 'name' + n.to_s, price: 100 + n, sort: 0 + n, is_hidden: false, photo: image_file)
 end
