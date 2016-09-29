f22fe5b74ea7b65da22f3e89b21b0049bbdfcc3a
nana <keyaki.436vrwgz@gmail.com>
2016-09-29 18:15:56 +0900

    herokuデプロイ検証用コミット

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    
     if user_signed_in?
       @order_item = current_order.order_items.new
     end
