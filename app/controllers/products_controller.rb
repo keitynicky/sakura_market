1ba6f9058228bcc5e87daa67b680a592812dde56
nana <keyaki.436vrwgz@gmail.com>
2016-09-29 18:12:44 +0900

    未ログイン状態のカート表示修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,9 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @order_item = current_order.order_items.new
+    if user_signed_in?
+      @order_item = current_order.order_items.new
+    end
   end
 
   def show_photo
