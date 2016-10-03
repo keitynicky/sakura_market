8cfbed6546b51889e0872fd6795389ad85c6ee85
nana <keyaki.436vrwgz@gmail.com>
2016-10-03 22:12:01 +0900

    ショッピングカート更新処理修正

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,7 +2,6 @@ class OrderItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_order
   before_action :set_order_item, only: [:create, :update]
-  after_action :after_create_or_update, only: [:create, :update]
 
   def index
   end
@@ -28,12 +27,9 @@ private
     @order_item = @order.order_items.find_or_initialize_by(product_id: order_item_params[:product_id])
     @order_item.quantity = order_item_params[:quantity]
     @order_item.save
-    redirect_to order_items_path            
-  end
-
-  def after_create_or_update
     @order.save
     session[:order_id] = @order.id
+    redirect_to order_items_path            
   end
 
   def order_item_params
