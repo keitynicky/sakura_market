a69428854f20654dad4e2e77d32912ae651b7915
nana <keyaki.436vrwgz@gmail.com>
2016-10-03 22:07:56 +0900

    seedデータ変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 
 10.times do |n|
-  Product.create!(name: 'name' + n.to_s, price: 100 + n, sort: 0 + n, is_hidden: false, photo: File.open(File.join(Rails.root, "/test/fixtures/files/image.png")))
+  product = Product.where(name: 'name' + n.to_s).first
+  if !product.nil?
+    product.destroy
+  end
+
+  Product.create!(name: 'name' + n.to_s, description: 'description' + n.to_s, price: 100 + n, sort: 0 + n, is_hidden: false, photo: File.open(File.join(Rails.root, "/test/fixtures/files/image.png")))
 end
