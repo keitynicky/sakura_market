77cec390f0959929ad64186267ad12387ca89b0b
nana <keyaki.436vrwgz@gmail.com>
2016-10-03 10:43:03 +0900

    削除リンク追加

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,15 +1,24 @@
 class OrderItemsController < ApplicationController
+  before_action :set_order
+
   def create
-    @order = current_order
     @order_item = @order.order_items.new(order_item_params)
     @order.save
     session[:order_id] = @order.id
+    redirect_to cart_path
+  end
 
+  def destroy
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.destroy
     redirect_to cart_path
   end
 
 private
-  
+  def set_order
+    @order = current_order
+  end  
+
   def order_item_params
     params.require(:order_item).permit(:quantity, :product_id)
   end
