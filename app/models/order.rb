526db964fe9ece90bfa73c40473463e89c78a0f8
nana <keyaki.436vrwgz@gmail.com>
2016-10-09 21:59:40 +0900

    不要なif文削除（order） orderのユニットテスト一部追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,40 +7,26 @@ class Order < ApplicationRecord
   SHIPPING_FEE = 600
 
   def subtotal
-    if has_order_items?
-      self[:subtotal] ||= order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
-    end
+    self[:subtotal] ||= order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
   end
 
   def tax
-    if has_order_items?
-     self[:tax] ||=  get_tax get_total_net_of_tax
-    end
+    self[:tax] ||=  get_tax get_total_net_of_tax
   end
 
   def cash_on_delivery
-    if has_order_items?
-     self[:cash_on_delivery] ||= get_cash_on_delivery 
-    end
+    self[:cash_on_delivery] ||= get_cash_on_delivery 
   end
 
   def shipping
-    if has_order_items?
-     self[:shipping] ||= get_shipping 
-    end
+    self[:shipping] ||= get_shipping 
   end
 
   def total
-    if has_order_items?
-     self[:total] ||= get_total 
-    end
+    self[:total] ||= get_total 
   end
 
   private
-
-  def has_order_items?
-    order_items.size > 0
-  end
 
   def get_cash_on_delivery
     CashOnDelivery.select_recorod_by_subtotal(subtotal).cost
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe `#subtotal` do
+    context `order_itemがまだ追加されていない時` do
+      it `初期値が設定されていること` do
+        order = Order.new
+        expect(order.subtotal).to eq(0)
+      end
+    end
+  end
+
+  describe `#cash_on_delivery` do
+    context `購入金額が10000円未満の時` do
+      it `300円となっていること` do
+        order = Order.new
+        order.subtotal = 10000 - 1        
+        expect(order.cash_on_delivery).to eq(300)
+      end
+    end
+    
+    context `購入金額が10000円の時` do
+      it `400円となっていること` do
+        order = Order.new
+        order.subtotal = 10000        
+        expect(order.cash_on_delivery).to eq(400)
+      end
+    end
+
+    context `購入金額が30,000円未満の時` do
+      it `400円となっていること` do
+        order = Order.new
+        order.subtotal = 30000 - 1        
+        expect(order.cash_on_delivery).to eq(400)
+      end
+    end
+
+    context `購入金額が30,000円の時` do
+      it `600円となっていること` do
+        order = Order.new
+        order.subtotal = 30000        
+        expect(order.cash_on_delivery).to eq(600)
+      end
+    end
+
+    context `購入金額が100,000円未満の時` do
+      it `600円となっていること` do
+        order = Order.new
+        order.subtotal = 100000 - 1        
+        expect(order.cash_on_delivery).to eq(600)
+      end
+    end
+
+    context `購入金額が100,000円の時` do
+      it `1000円となっていること` do
+        order = Order.new
+        order.subtotal = 100000        
+        expect(order.cash_on_delivery).to eq(1000)
+      end
+    end
+
+  end
 end
