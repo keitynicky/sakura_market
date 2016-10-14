5f273fc10c19315bc4f48302646a756aea20461d
nana <keyaki.436vrwgz@gmail.com>
2016-10-14 21:44:15 +0900

    progress処理helperを使って共通化

--- app/helpers/check_outs_helper.rb
+++ app/helpers/check_outs_helper.rb
@@ -1,2 +1,36 @@
 module CheckOutsHelper
+  def get_progress_info current_process
+    ProgressInfo.new(current_process)     
+  end
+  
+  class ProgressInfo < Struct.new :current_process
+    
+    def one_class
+      get_color_class current_process >= 1
+    end
+
+    def two_class
+      get_color_class current_process >= 2
+    end
+
+    def three_class
+      get_color_class current_process >= 3
+    end
+
+    def four_class
+      get_color_class current_process >= 4
+    end
+
+    def progress_width
+        width_percent = (20 * current_process) + 5
+        "width: #{width_percent}%"
+    end
+
+  private
+
+    def get_color_class is_primary
+      is_primary ? "primary-color" : "no-color"
+    end
+  end
+
 end
