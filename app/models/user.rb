34ba9b144909195fc7bf734743ac38894efdd91e
nana <keyaki.436vrwgz@gmail.com>
2016-10-15 15:03:10 +0900

    お届先住所の表示機能組み込み

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def disp_address
+    "#{self.address_level1} #{self.address_level2} #{self.address_line1} #{self.address_line2}"
+  end
+
+  def disp_user_name
+    "#{self.family_name} #{self.given_name}様"
+  end
 end
