d4eef294419778f586a07874c196a6e249874f23
nana <keyaki.436vrwgz@gmail.com>
2016-10-19 12:52:26 +0900

    数値表示のフォーマット指定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def disp_money target
     number_to_currency(target, :format => "%u%n", :unit => "￥", :precision => 0)
   end
+
+  def disp_number target
+    number_with_delimiter(target, :delimiter => ',')
+  end
 end
