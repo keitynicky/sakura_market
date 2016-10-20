284dc3ef12fd4abbe1d568dafea7a0e0d831b969
nana <keyaki.436vrwgz@gmail.com>
2016-10-20 20:35:44 +0900

    rubocop対応

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,8 +5,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    if user_signed_in?
-      @order_item = current_order.order_items.find_or_initialize_by(product_id: params[:id])
-    end
+    return unless user_signed_in?
+    @order_item = current_order.order_items.find_or_initialize_by(product_id: params[:id])
   end
 end
