6c0554c559d51beb0cf0e0adbf76cf7e0c60b2f2
nana <keyaki.436vrwgz@gmail.com>
2016-10-20 20:38:15 +0900

    rubocop対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def disp_money target
-    number_to_currency(target, :format => "%u%n", :unit => "￥", :precision => 0)
+  def disp_money(target)
+    number_to_currency(target, format: '%u%n', unit: '￥', precision: 0)
   end
 
-  def disp_number target
-    number_with_delimiter(target, :delimiter => ',')
+  def disp_number(target)
+    number_with_delimiter(target, delimiter: ',')
   end
 end
