6374cfed2d533f59f5e3f9721eb1c24a4cecb32a
nana <keyaki.436vrwgz@gmail.com>
2016-10-22 11:27:53 +0900

    CashOnDeliveryのユニットテスト実装

--- spec/models/cash_on_delivery_spec.rb
+++ spec/models/cash_on_delivery_spec.rb
@@ -1,6 +1,51 @@
-require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe CashOnDelivery, type: :model do
-  # OrderItemのユニットテストでこのパターンのテストを実施しているので、ここではテストせず
+  describe `#select_recorod_by_subtotal` do
+    context `購入金額が10000円未満の時` do
+      it `300円となっていること` do
+        expect(CashOnDelivery.select_recorod_by_subtotal(10000 - 1).cost).to eq(300)
+      end
+    end
+  end
+
+  describe `#select_recorod_by_subtotal` do
+    context `購入金額が10000円の時` do
+      it `400円となっていること` do
+        expect(CashOnDelivery.select_recorod_by_subtotal(10000).cost).to eq(400)
+      end
+    end
+  end
+
+  describe `#select_recorod_by_subtotal` do
+    context `購入金額が30,000円未満の時` do
+      it `400円となっていること` do
+        expect(CashOnDelivery.select_recorod_by_subtotal(30000 - 1).cost).to eq(400)
+      end
+    end
+  end
+
+  describe `#select_recorod_by_subtotal` do
+    context `購入金額が30,000円の時` do
+      it `600円となっていること` do
+        expect(CashOnDelivery.select_recorod_by_subtotal(30000).cost).to eq(600)
+      end
+    end
+  end
+
+  describe `#select_recorod_by_subtotal` do
+    context `購入金額が100,000円未満の時` do
+      it `600円となっていること` do
+        expect(CashOnDelivery.select_recorod_by_subtotal(100000 - 1).cost).to eq(600)
+      end
+    end
+  end
+
+  describe `#select_recorod_by_subtotal` do
+    context `購入金額が100,000円の時` do
+      it `1000円となっていること` do
+        expect(CashOnDelivery.select_recorod_by_subtotal(100000).cost).to eq(1000)
+      end
+    end
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -14,53 +14,12 @@ RSpec.describe Order, type: :model do
 
   describe `#cash_on_delivery` do
     context `購入金額が10000円未満の時` do
-      it `300円となっていること` do
+      it `300円となっていること(各境界値のテストは、CashOnDeliveryのspecに記載)` do
         order = Order.new
         order.subtotal = 10000 - 1        
         expect(order.cash_on_delivery).to eq(300)
       end
     end
-    
-    context `購入金額が10000円の時` do
-      it `400円となっていること` do
-        order = Order.new
-        order.subtotal = 10000        
-        expect(order.cash_on_delivery).to eq(400)
-      end
-    end
-
-    context `購入金額が30,000円未満の時` do
-      it `400円となっていること` do
-        order = Order.new
-        order.subtotal = 30000 - 1        
-        expect(order.cash_on_delivery).to eq(400)
-      end
-    end
-
-    context `購入金額が30,000円の時` do
-      it `600円となっていること` do
-        order = Order.new
-        order.subtotal = 30000        
-        expect(order.cash_on_delivery).to eq(600)
-      end
-    end
-
-    context `購入金額が100,000円未満の時` do
-      it `600円となっていること` do
-        order = Order.new
-        order.subtotal = 100000 - 1        
-        expect(order.cash_on_delivery).to eq(600)
-      end
-    end
-
-    context `購入金額が100,000円の時` do
-      it `1000円となっていること` do
-        order = Order.new
-        order.subtotal = 100000        
-        expect(order.cash_on_delivery).to eq(1000)
-      end
-    end
-
   end
 
   describe `#shipping` do
