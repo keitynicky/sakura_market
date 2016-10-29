ac880afc812cd275d76e633f2670e4ad388a42e9
nana <keyaki.436vrwgz@gmail.com>
2016-10-29 21:28:14 +0900

    rubocop対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :products, only: [:show] do
-    member {get :show_photo}
+    member { get :show_photo }
   end
   resource :account, only: [:show]
   resources :order_items, only: [:index, :create, :update, :destroy]
   resource :check_out, only: [:update, :show] do
-    member {get :user_info}
-    member {put :update_user_info}
-    member {get :delivery}
-    member {get :confirm}
-    member {get :complete}
+    member { get :user_info }
+    member { put :update_user_info }
+    member { get :delivery }
+    member { get :confirm }
+    member { get :complete }
   end
-  root to: "products#index"
+  root to: 'products#index'
 
   namespace :admin do
     resources :products
     resources :users, except: [:new, :create]
   end
 end
-
