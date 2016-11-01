b8b5a157cde54e5dd43f782df1690fc20af34ad2
nana <keyaki.436vrwgz@gmail.com>
2016-11-01 14:36:25 +0900

    誤記修正

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -11,7 +11,7 @@ class Admin::ProductsController < Admin::AdminController
   def create
     @product.update_attributes(product_params)
     @product.photo = File.open(params[:product][:photo].path, 'rb').read
-    q if @product.save
+    if @product.save
       redirect_to admin_products_path
     else
       render :new
