0bf01693ec12c2aa9c40e489403f4fe2b22981e7
nana <keyaki.436vrwgz@gmail.com>
2016-11-01 15:52:49 +0900

    商品フォーム共通化 画像ファイル未入力時の挙動追加

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < Admin::AdminController
   before_action :init_product, only: [:new, :create]
+  before_action :set_product, only: [:edit, :update]
 
   def index
     @products = Product.all
@@ -10,12 +11,20 @@ class Admin::ProductsController < Admin::AdminController
 
   def create
     @product.update_attributes(product_params)
-    @product.photo = File.open(params[:product][:photo].path, 'rb').read
+    unless params[:product][:photo].nil?
+      @product.photo = File.open(params[:product][:photo].path, 'rb').read
+    end
     if @product.save
       redirect_to admin_products_path
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   private
@@ -24,7 +33,11 @@ class Admin::ProductsController < Admin::AdminController
     @product = Product.new
   end
 
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
   def product_params
-    params.require(:product).permit(:name, :description, :price, :sort, :photo)
+    params.require(:product).permit(:name, :description, :price, :sort)
   end
 end
